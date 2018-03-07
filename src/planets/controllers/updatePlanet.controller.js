f092dee7fadf9831f7b2aa931b24e6a75db6acca
mjaskot <jaskotmarcin1@gmail.com>
2018-03-07 17:09:14 +0100

    Fixed typo in planets controller

--- src/planets/controllers/updatePlanet.controller.js
+++ src/planets/controllers/updatePlanet.controller.js
@@ -4,12 +4,13 @@ const { planetModel } = require("../planet.model");
 
 function updatePlanet(req, res, next) {
   const id = req.params.id;
-  planetModel.findByIdAndUpdate({ _id: id }, res.body, (err, planet) => {
-    if (err)
+  planetModel.findByIdAndUpdate(id, req.body, (err, planet) => {
+    if (err) {
       return res.status(500).json({
         message: "There was an error processing your request",
         stack: err.stack
       });
+    }
     return res.status(200).json(planet);
   });
 }
