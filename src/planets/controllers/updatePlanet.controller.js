34adebdcfd68850b2ca8f61b27a29d1cd6e15aa0
mjaskot <jaskotmarcin1@gmail.com>
2018-03-09 15:24:36 +0100

    Update planet now returns new updated version of object instead of old

--- src/planets/controllers/updatePlanet.controller.js
+++ src/planets/controllers/updatePlanet.controller.js
@@ -4,15 +4,20 @@ const { planetModel } = require("../planet.model");
 
 function updatePlanet(req, res, next) {
   const id = req.params.id;
-  planetModel.findByIdAndUpdate(id, req.body, (err, planet) => {
-    if (err) {
-      return res.status(500).json({
-        message: "There was an error processing your request",
-        stack: err.stack
-      });
-    }
-    return res.status(200).json(planet);
-  });
+  planetModel.findByIdAndUpdate(
+    id,
+    req.body,
+    (err, planet) => {
+      if (err) {
+        return res.status(500).json({
+          message: "There was an error processing your request",
+          stack: err.stack
+        });
+      }
+      return res.status(200).json(planet);
+    },
+    { new: true }
+  );
 }
 
 module.exports = {
