6313ca930ee3a9895dc9e07ac5bbc3c3f637f08e
mjaskot <jaskotmarcin1@gmail.com>
2018-03-09 15:23:56 +0100

    Implemented comment update endpoint

--- src/planets/comments/controllers/updateComment.controller.js
+++ src/planets/comments/controllers/updateComment.controller.js
@@ -1,2 +1,33 @@
-const planetModel = require("../../planet.model");
-const planetModel = require("../../planet.model");
+const { planetModel } = require("../../planet.model");
+const mongoose = require("mongoose");
+
+function updateComment(commentId, comment) {
+  return planetModel
+    .findOneAndUpdate(
+      { "comments._id": commentId },
+      {
+        "comments.$.content": comment.content,
+        "comments.$.updatedAt": new Date()
+      },
+      { new: true }
+    )
+    .exec();
+}
+
+async function putCommentController(req, res, next) {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  try {
+    const comment = await updateComment(commentId, {
+      content
+    });
+    return res.status(201).json({ comment });
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = {
+  putCommentController
+};
